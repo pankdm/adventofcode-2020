a9c8ae6d787167626d90e35f1e77bd551928ee14
Dmitry Panin <pank.dm@gmail.com>
2020-12-05 21:11:23 -0800

    [Day 6] Initial solution: #1911/#839

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -12,16 +12,87 @@ use serde_scan;
 extern crate aoc;
 use aoc::*;
 
+
+fn process_group(g: &Vec<String>) -> i64 {
+    // println!("g = {:?}", g);
+    let mut r = HashMap::new();
+    for line in g {
+        for c in line.chars() {
+            r.insert(c, 1);
+        }
+    }
+    r.len() as i64
+}
+
 pub fn part1(lines: &Vec<String>) -> i64 {
     let mut res = 0;
-    // TODO: code here
-    -1
+    let mut index = 0;
+
+    let mut group = Vec::new();
+    loop {
+        let line = if index < lines.len() {
+            lines[index].clone()
+        } else {
+            "".to_string()
+        };
+        if line.is_empty() {
+            res += process_group(&group);
+            group = Vec::new();
+        } else {
+            group.push(line);
+        }
+        if index >= lines.len() {
+            break
+        }
+        index += 1;
+    }
+    res as i64
 }
 
 pub fn part2(lines: &Vec<String>) -> i64 {
     let mut res = 0;
-    // TODO: code here
-    -1
+    let mut index = 0;
+
+    let mut group = Vec::new();
+    loop {
+        let line = if index < lines.len() {
+            lines[index].clone()
+        } else {
+            "".to_string()
+        };
+        if line.is_empty() {
+            res += process_group2(&group);
+            group = Vec::new();
+        } else {
+            group.push(line);
+        }
+        if index >= lines.len() {
+            break
+        }
+        index += 1;
+    }
+    res as i64
+}
+
+fn process_group2(g: &Vec<String>) -> i64 {
+    // println!("g = {:?}", g);
+    let mut r = HashMap::new();
+    for line in g {
+        for c in line.chars() {
+            if !r.contains_key(&c) {
+                r.insert(c, 1);
+            } else {
+                r.insert(c, r[&c] + 1);
+            }
+        }
+    }
+    let mut res = 0;
+    for (k, v) in r.iter() {
+        if *v as i64 == g.len() as i64 {
+            res += 1;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
@@ -32,13 +103,13 @@ mod tests {
     #[test]
     fn test_part1() {
         let lines = read_main_input();
-        assert_eq!(part1(&lines), -1);
+        assert_eq!(part1(&lines), 6763);
     }
 
     #[test]
     fn test_part2() {
         let lines = read_main_input();
-        assert_eq!(part2(&lines), -1);
+        assert_eq!(part2(&lines), 3512);
     }
 }
 
@@ -51,5 +122,5 @@ fn main() {
     let lines = read_main_input();
 
     println!("part1 = {}", part1(&lines));
-    // println!("part2 = {}", part2(&lines));
+    println!("part2 = {}", part2(&lines));
 }
