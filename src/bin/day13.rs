4fc68578d6c17713039662a847a3d25ce0a8b506
Dmitry Panin <pank.dm@gmail.com>
2020-12-13 11:28:42 -0800

    [Day 13] Clean up a little bit

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -5,7 +5,6 @@
 #![allow(unused_mut)]
 
 // Some basic includes to alwawys include
-use num_bigint::*;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::env;
 use std::fs;
@@ -14,6 +13,8 @@ use serde_scan;
 
 extern crate aoc;
 use aoc::*;
+
+use num_bigint::*;
 
 pub fn part1(lines: &Vec<String>) -> i64 {
     let mut res = 0;
@@ -39,37 +40,44 @@ pub fn part1(lines: &Vec<String>) -> i64 {
     -1
 }
 
-fn gcd(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt) {
-    if a == (0 as i64).to_bigint().unwrap() {
-        return (b.clone(), 0.to_bigint().unwrap(), 1.to_bigint().unwrap());
+mod my_bigint {
+    use num_bigint::*;
+    fn gcd(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt) {
+        if a == (0 as i64).to_bigint().unwrap() {
+            return (b.clone(), 0.to_bigint().unwrap(), 1.to_bigint().unwrap());
+        }
+        let (d, x1, y1) = gcd(b.clone() % a.clone(), a.clone());
+        let x = y1 - (b / a) * x1.clone();
+        let y = x1;
+        return (d, x, y);
     }
-    let (d, x1, y1) = gcd(b.clone() % a.clone(), a.clone());
-    let x = y1 - (b / a) * x1.clone();
-    let y = x1;
-    return (d, x, y);
-}
 
-fn abs_bigint(a: &BigInt) -> BigInt {
-    if *a < (0 as i64).to_bigint().unwrap() {
-        return (-1 as i64).to_bigint().unwrap() * a;
+    fn abs_bigint(a: &BigInt) -> BigInt {
+        if *a < (0 as i64).to_bigint().unwrap() {
+            return (-1 as i64).to_bigint().unwrap() * a;
+        }
+        a.clone()
     }
-    a.clone()
-}
 
-fn find_any_solution(a: &BigInt, b: &BigInt, c: &BigInt) -> Option<(BigInt, BigInt, BigInt)> {
-    let (g, mut x0, mut y0) = gcd(abs_bigint(&a), abs_bigint(&b));
-    if c % g.clone() != (0 as i64).to_bigint().unwrap() {
-        return None;
+    pub fn find_any_solution(
+        a: &BigInt,
+        b: &BigInt,
+        c: &BigInt,
+    ) -> Option<(BigInt, BigInt, BigInt)> {
+        let (g, mut x0, mut y0) = gcd(abs_bigint(&a), abs_bigint(&b));
+        if c % g.clone() != (0 as i64).to_bigint().unwrap() {
+            return None;
+        }
+        x0 *= c / g.clone();
+        y0 *= c / g.clone();
+        if *a < (0 as i64).to_bigint().unwrap() {
+            x0 *= -1;
+        }
+        if *b < (0 as i64).to_bigint().unwrap() {
+            y0 *= -1;
+        }
+        return Some((x0, y0, g));
     }
-    x0 *= c / g.clone();
-    y0 *= c / g.clone();
-    if *a < (0 as i64).to_bigint().unwrap() {
-        x0 *= -1;
-    }
-    if *b < (0 as i64).to_bigint().unwrap() {
-        y0 *= -1;
-    }
-    return Some((x0, y0, g));
 }
 
 pub fn part2_impl(line: &str) -> String {
@@ -84,46 +92,29 @@ pub fn part2_impl(line: &str) -> String {
         }
     }
 
-    // buses = [(29, 0), (41, 19), (521, 29), (23, 37), (13, 42), (17, 46), (601, 60), (37, 66), (19, 79)]
-    // buses = [(7, 0), (13, 1), (59, 4), (31, 6), (19, 7)]
-
     let mut buses = Vec::new();
     let mut ans = 1 as i64;
     for (x, offset) in tmp.iter() {
         let r = ((x - offset) % x + x) % x;
         buses.push((x.clone(), r.clone()));
-        // println!("A = {} mod {}", r, x);
     }
-    // }
-    // 7x
-    // 7x + 1 % 13
-    // 7x + 1 = 13y
-    // 13y - 1
-    // 7 * (13a - 1) * (59b - 4) * (31c - 6) * (19d - 7) * d
 
     let mut a = (1 as i64).to_bigint().unwrap();
     let mut A = (0 as i64).to_bigint().unwrap();
 
-    // println!("{:?}", find_any_solution(7, -13, 12));
     for (b, B) in buses.iter() {
+        // find_any_solution(a, b, c) -> (g, x, y) solves and equation so that
+        // a * x + b * y = c
+        // Here we find a solution to a * x + A = b * y + B
+        // which is translated to a * x - b * y = B - A
         let (x0, y0, g) =
-            find_any_solution(&a.clone(), &(-b.clone()), &(B.clone() - A.clone())).unwrap();
-        // assert!(g == 1, "g={}", g);
-        // x = x0 + k * b
-        // a * x + A
-        // a * (x0 + k * b) + A
-        // k * a * b + a * x0 + A
+            my_bigint::find_any_solution(&a.clone(), &(-b.clone()), &(B.clone() - A.clone()))
+                .unwrap();
         A = x0 * a.clone() + A.clone();
         a = a.clone() * b;
-        // println!("    After ({}, {}) -> {} * x + {}", b, B, a, A);
     }
-    // let (g, _, _) = gcd(a, A);
     let k = -A.clone() / a.clone();
-    // println!("  k = {}", k);
     let res = k * a + A;
-    // println!("{}", res);
-
-    // (a * x + A) = (b * y + B)
     format!("{}", res)
 }
 
